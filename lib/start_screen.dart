5fbbf8eda20848b75c84b03601d9cd926f9cfda4
sayedbintaher <sayedbintaher@gmail.com>
2024-04-22 00:40:20 +0600

    flutter quiz app first commit

---
+++ lib/start_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  @override
+  Widget build(context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+            color: const Color.fromARGB(150, 255, 255, 255),
+          ),
+          const SizedBox(
+            height: 80,
+          ),
+          const Text(
+            "Learn flutter the fun way!",
+            style: TextStyle(
+              fontSize: 25,
+              color: Color.fromARGB(255, 237, 223, 252),
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          OutlinedButton.icon(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
+            icon: const Icon(Icons.arrow_right_alt),
+            label: const Text('Start Quiz'),
+          )
+        ],
+      ),
+    );
+  }
+}
